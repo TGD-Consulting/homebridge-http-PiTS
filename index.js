01529c9fad4235ca7384434c5122d2c83a6a045c
TGD-Consulting <info@tgd-consulting.de>
2017-01-19 01:09:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ module.exports = function (homebridge) {
     EveWeatherService = function(displayName, subtype) {
         Service.call(this, displayName, 'E863F001-079E-48FF-8F27-9C2605A29F52', subtype);
         // Required Characteristics
-        this.addCharacteristic(Characteristic.CurrentTemperatureEveAirPressure);
+        this.addCharacteristic(Characteristic.CurrentTemperature);
         // Optional Characteristics
         this.addOptionalCharacteristic(Characteristic.CurrentRelativeHumidity);
         this.addOptionalCharacteristic(EveAirPressure);
@@ -68,14 +68,17 @@ function HttpPiTS(log, config) {
     this.url = config["url"];
     this.http_method = config["http_method"] || "GET";
     this.name = config["name"];
+    this.service = config["service"];
     this.manufacturer = config["manufacturer"] || "TGD-Consulting";
     this.model = config["model"] || "PiTS-It!";
     this.serial = config["serial"] || "PiTS Serial";
-    this.humidity = config["humidity"];
-    this.pressure = config["pressure"];
+    this.valueTemperature = config["valueTemperature"];
+    this.valueHumidity = config["valueHumidity"];
+    this.valueAirPressure = config["valueAirPressure"];
 
 	this.temperatureService;
 	this.humidityService;
+        this.pressureService;
 }
 
 HttpPiTS.prototype = {
@@ -92,65 +95,118 @@ HttpPiTS.prototype = {
                 })
     },
 
-    getStateHumidity: function(callback){
-	callback(null, this.humidity);
+    getCurrentHumidity: function(callback){
+	callback(null, this.valueHumidity);
     },
 
-    getState: function (callback) {
+    getCurrentAirPressure: function(callback){
+        callback(null, this.valueAirPressure);
+    },
+
+    getCurrent: function (callback) {
         var body;
 
 	var res = request(this.http_method, this.url, {});
 	if(res.statusCode > 400){
-	  this.log('HTTP power function failed');
+	  this.debug && this.log('HTTP power function failed');
 	  callback(error);
 	} else {
-	  this.log('HTTP power function succeeded!');
+	  this.debug && this.log('HTTP power function succeeded!');
           var info = JSON.parse(res.body);
 
-          temperatureService.setCharacteristic(Characteristic.CurrentTemperature, info.temperature);
-          if(this.humidity !== false)
+          if(this.valueTemperature !== false)
+            temperatureService.setCharacteristic(Characteristic.CurrentTemperature, info.temperature);
+          if(this.valueHumidity !== false)
             humidityService.setCharacteristic(Characteristic.CurrentRelativeHumidity, info.humidity);
+          if(this.valueAirPressure !== false)
+            pressureService.setCharacteristic(EveAirPressure, info.airpressure);
 
           this.log(res.body);
           this.log(info);
 
-          this.temperature = info.temperature;
-          if(this.humidity !== false)
-            this.humidity = info.humidity;
+          if(this.valueTemperature !== false)
+            this.valueTemperature = info.temperature;
+          if(this.valueHumidity !== false)
+            this.valueHumidity = info.humidity;
+          if(this.valueAirPressure !== false)
+            this.valueAirPressure = info.airpressure;
 
 	  callback(null, this.temperature);
 	}
     },
 
     identify: function (callback) {
-        this.log("Identify requested!");
+        this.debug && this.log("Identify requested!");
         callback(); // success
     },
 
     getServices: function () {
         this.debug && this.log("getServices");
-        var services = [],
-            informationService = new Service.AccessoryInformation();
+        var informationService = new Service.AccessoryInformation();
 
         informationService
-                .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
-                .setCharacteristic(Characteristic.Model, this.model)
-                .setCharacteristic(Characteristic.SerialNumber, this.serial);
-        services.push(informationService);
+                .setCharacteristic(Characteristic.Manufacturer, 'PiTS: ' + this.manufacturer)
+                .setCharacteristic(Characteristic.Model, 'PiTS: ' + this.model)
+                .setCharacteristic(Characteristic.SerialNumber, 'PiTS sensor: ' + this.serial);
+        // services.push(informationService);
 
-        temperatureService = new Service.TemperatureSensor(this.name);
-        temperatureService
-                .getCharacteristic(Characteristic.CurrentTemperature)
-                .on('get', this.getState.bind(this));
-        services.push(temperatureService);
+        var services = [informationService];
 
-        if(this.humidity !== false){
-          humidityService = new Service.HumiditySensor(this.name);
-          humidityService
-                  .getCharacteristic(Characteristic.CurrentRelativeHumidity)
-                  .setProps({minValue: -100, maxValue: 100})
-                  .on('get', this.getStateHumidity.bind(this));
-          services.push(humidityService);
+        // Create primary service
+        switch (this.service) {
+
+            case "TemperatureSensor":
+                this.primaryservice = new Service.TemperatureSensor(this.name);
+                this.primaryservice.getCharacteristic(Characteristic.CurrentTemperature)
+                .setProps({ minValue: -55, maxValue: 125 })
+                    .on('get', this.getCurrent.bind(this));
+                break;
+
+            case "HumiditySensor":
+                this.primaryservice = new Service.HumiditySensor(this.name);
+                this.primaryservice.getCharacteristic(Characteristic.CurrentRelativeHumidity)
+                    .on('get', this.getCurrentHumidity.bind(this));
+                break;
+
+            case "FakeEveWeatherSensor":
+                this.primaryservice = new EveWeatherService("Eve Weather");
+                this.primaryservice.getCharacteristic(Characteristic.CurrentTemperature)
+                    .on('get', this.getCurrent.bind(this));
+                break;
+
+            case "FakeEveWeatherSensorWithLog":
+                this.primaryservice = new EveWeatherService("Eve Weather");
+                this.primaryservice.getCharacteristic(Characteristic.CurrentTemperature)
+                    .on('get', this.getCurrent.bind(this));
+                break;
+
+            default:
+                this.debug && this.log.warn('WARN: Service %s %s unknown, skipping...', this.service, this.name);
+                break;
+        }
+
+        services = services.concat(this.primaryservice);
+        if (services.length === 1) {
+            this.debug this.log.warn("WARN: Only the InformationService was successfully configured for " + this.name + "! No device services available!");
+            return services;
+        }
+
+        var service = services[1];
+
+        // Add optional characteristics...
+        if (this.valueTemperature && (this.service != "TemperatureSensor") && (this.service != "FakeEveWeatherSensor") && (this.service != "FakeEveWeatherSensorWithLog")) {
+            service.addCharacteristic(Characteristic.CurrentTemperature)
+            .setProps({ minValue: -55, maxValue: 125 })
+                .on('get', this.getCurrent.bind(this));
+        }
+        if (this.config.valueHumidity && (this.service != "HumiditySensor")) {
+            service.addCharacteristic(Characteristic.CurrentRelativeHumidity)
+            .setProps({minValue: 0, maxValue: 100})
+                .on('get', this.getCurrentRelativeHumidity.bind(this));
+        }
+        if (this.config.valueAirPressure && (this.service != "AirPressureSensor") && (this.service != "PressureSensor")) {
+            service.addCharacteristic(EveAirPressure)
+                .on('get', this.getCurrentAirPressure.bind(this));
         }
 
         return services;
